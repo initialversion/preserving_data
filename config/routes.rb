41f10af2e00c54df147ee0f072290913f5495ecc
Raghu Betina <raghu@firstdraft.com>
2016-10-26 13:06:25 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "statuses#index"
   # Routes for the Status resource:
   # CREATE
